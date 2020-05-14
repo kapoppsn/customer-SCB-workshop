659c8af9d799312496cebb3552364ca54a95a3ed
Pasinee Wasupan <kapopwsp@gmail.com>
2020-05-14 16:49:28 +0700

    first commit

---
+++ src/main/java/com/digitalacademy/controller/CustomerController.java
@@ -0,0 +1,68 @@
+package com.digitalacademy.controller;
+
+import com.digitalacademy.model.Customer;
+import com.digitalacademy.service.CustomerService;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/customer")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/list")
+    public List<Customer> getAllCustomer(){
+        return customerService.getCustomerList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
+        Customer customer = customerService.getCustomer(id);
+        if(customer != null){
+            return ResponseEntity.ok(customer);
+
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(params = "name")
+    public ResponseEntity<?> getCustomerByName(@RequestParam(value = "name") String name){
+        List<Customer> customers = customerService.getCustomerName(name);
+        return customers != null && !customers.isEmpty() ? ResponseEntity.ok(customers) : ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> createCustomer(@Valid @RequestBody Customer body){
+        Customer customer = customerService.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putCustomer(@PathVariable Long id, @Valid @RequestBody Customer body){
+        body.setId(id);
+        Customer customer = customerService.updateCustomer(id, body);
+        return customer != null ? ResponseEntity.ok(customer)
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable Long id){
+        return customerService.deleteByID(id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/digitalacademy/model/Customer.java
@@ -0,0 +1,41 @@
+package com.digitalacademy.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+
+    @NotNull
+    @Size(min = 1 , max = 100 , message = "Please type your first name")
+    private  String firstName;
+
+    @NotNull
+    @Size(min = 1 , max = 100 , message = "Please type your last name")
+    private  String lastName;
+
+
+    @Email( message = "Please type your valid email")
+    private  String email;
+
+    @NotNull
+    private String phoneNo;
+
+
+    private Integer age;
+
+
+}
